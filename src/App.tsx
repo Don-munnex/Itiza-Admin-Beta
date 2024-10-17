170ca019997c21d305cc62d81fb4adf2429866cc
Don-munnex <muniradeiza01@gmail.com>
2024-10-17 02:37:13 +0100

    i removed unused imports

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,10 @@
-import React from 'react'
+
 // import './App.css'
 import PinkDiv from './components/PinkDiv'
-import PUPPY from './components/PUPPY'
+
 import Form from './components/Form'
 import WriteUp from './components/WriteUp'
 import Footer from './components/Footer'
-import WalletContextProvider from './components/connection'
-import puppy from "./Images/puppy.jpg"
 
 function App() {
   return (
